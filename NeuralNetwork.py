5ab0faa535debd16abe337a2c4ab61610b448cc4
Ian Titor <ianpatricktan@gmail.com>
2021-05-04 22:05:28 -0400

    worked more on it, got the fully connected layer complete

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -30,11 +30,37 @@ def karges(kwargs, defaults):
     return [vars[x] for x in list(defaults)]
 
 
+def generateWeights(layerData, **kwargs):
+    minimum, maximum = karges(kwargs, {'minimum': -1, 'maximum': 1})
+    return [np.random.uniform(minimum, maximum, [layerData[1:][layer], layerData[layer] + 1]) for layer in range(len(layerData) - 1)]
+
+
 def imageSplit(image, kernelShape):
         imageShape = list(image.shape)
         return np.moveaxis(np.array(np.split(np.moveaxis(np.array(np.split(image, len(image[0, 0]) / kernelShape[1], axis = 2)), 0, 1), len(image[0]) / kernelShape[0], axis = 2)), 0, 1)
 
 
+class Model:
+    def __init__(self):
+
+
 class FullyConnected:
-    def __init__(self, weights, actFunc):
+    def __init__(self, weights, actFunc = sigmoid):
         self.weights, self.actFunc = weights, actFunc
+
+
+    def layer(self, inputs, **kwargs):
+        weights, actFunc = karges(kwargs, {'weights': self.weights, 'actFunc': self.actFunc})
+        return actFunc(np.tensordot(np.append(inputs, 1), weights, axis = [-1, -1]))
+
+
+    def gradient(self, inputs, outputs, **kwargs):
+        actFunc, actFuncDeriv = karges(kwargs, {'actFunc': sigmoid, 'actFuncDeriv': sigmoidD})
+        weightedSum = self.layer(inputs, actFunc = lol, **kwargs)
+        layerOutputs = actFunc(weightedSum)
+        chainDerivCoef = 2 * (layerOutputs - outputs) * actFuncD(weightedSum)
+        inputsGrad = np.sum(weights.transpose()[:-1] * chainDerivCoef, axis = -1)
+        weightsGrad = np.outer(np.append(inputs, 1), chainDerivCoef).transpose()
+        return inputsGrad, weightsGrad
+
+
